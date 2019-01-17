16ad701e1038c00fe648d14820566f2be59735a2
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-17 16:36:12 -0500

    modified test purchase item

--- TestVendorPt1.sol
+++ TestVendorPt1.sol
@@ -11,12 +11,13 @@ contract TestVendorPt1 is VendorPt1 {
     addItem("Mugs", 5);
     addItem("Ties", 30);
     addItem("Scarves", 20);
-    addItem("Sunglasses", 40);
     assert(getItemCount("Scarves") == 20);
   }
 
   function test_PurchaseItem() {
-    test_AddItem();
+    addItem("Watches", 5);
+    addItem("Sunglasses", 40);
+    addItem("Water Bottles", 18);
     assert(getItemCount("Sunglasses") == 40);
     purchaseItem("Sunglasses");
     assert(getItemCount("Sunglasses") == 39);
