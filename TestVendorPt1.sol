4ba5c1249b71302f839c034adfd872304a9e0f0a
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-22 16:26:41 -0500

    Made the testing use more gas

--- TestVendorPt1.sol
+++ TestVendorPt1.sol
@@ -18,8 +18,18 @@ contract TestVendorPt1 is VendorPt1 {
     addItem("Watches", 5);
     addItem("Sunglasses", 40);
     addItem("Water Bottles", 18);
+    addItem("Card", 100);
+    addItem("Computer", 18);
+    addItem("Head Band", 20);
+    addItem("Keyboard", 30);
+    addItem("T shirts", 100);
+    addItem("Sodas", 50);
+    addItem("Marbles", 450);
     assert(getItemCount("Sunglasses") == 40);
     purchaseItem("Sunglasses");
     assert(getItemCount("Sunglasses") == 39);
+    assert(getItemCount("Marbles") == 450);
+    purchaseItem("Marbles");
+    assert(getItemCount("Marbles") == 449);
   }
 }
